1737621ac568e583be76fe3edd2f6d5d343a2263
Duc Nguyen <minhduc140583@gmail.com>
2020-06-13 11:04:49 +0700

    Add GenericService

---
+++ generic_service.go
@@ -0,0 +1,12 @@
+package service
+
+import "context"
+
+type GenericService interface {
+	ViewService
+	Insert(ctx context.Context, model interface{}) (int64, error)
+	Update(ctx context.Context, model interface{}) (int64, error)
+	Patch(ctx context.Context, model map[string]interface{}) (int64, error)
+	Save(ctx context.Context, model interface{}) (int64, error)
+	Delete(ctx context.Context, id interface{}) (int64, error)
+}
