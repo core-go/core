2eddae799d7636800de8802a62511eb08de0596f
Duc Nguyen <minhduc140583@gmail.com>
2020-06-13 11:03:40 +0700

    Add ViewService

---
+++ view_service.go
@@ -0,0 +1,11 @@
+package service
+
+import "context"
+
+type ViewService interface {
+	Ids() []string
+	All(ctx context.Context) (interface{}, error)
+	Load(ctx context.Context, id interface{}) (interface{}, error)
+	LoadAndDecode(ctx context.Context, id interface{}, result interface{}) (bool, error)
+	Exist(ctx context.Context, id interface{}) (bool, error)
+}
