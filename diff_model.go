58ec377597c15b795f6a03b248d3b9079538ca95
Duc Nguyen <minhduc140583@gmail.com>
2020-07-12 23:25:24 +0700

    Refactor DiffModel

--- diff_model.go
+++ diff_model.go
@@ -1,7 +1,7 @@
 package service
 
 type DiffModel struct {
-	Id       string `json:"id,omitempty" gorm:"column:id" bson:"_id,omitempty" dynamodbav:"id,omitempty" firestore:"id,omitempty"`
-	OldValue string `json:"oldValue,omitempty" gorm:"column:oldvalue" bson:"oldValue,omitempty" dynamodbav:"oldValue,omitempty" firestore:"oldValue,omitempty"`
-	NewValue string `json:"newValue,omitempty" gorm:"column:newvalue" bson:"newValue,omitempty" dynamodbav:"newValue,omitempty" firestore:"newValue,omitempty"`
+	Id     interface{} `mapstructure:"id" json:"id,omitempty" gorm:"column:id" bson:"_id,omitempty" dynamodbav:"id,omitempty" firestore:"id,omitempty"`
+	Origin interface{} `mapstructure:"origin" json:"origin,omitempty" gorm:"column:origin" bson:"origin,omitempty" dynamodbav:"origin,omitempty" firestore:"origin,omitempty"`
+	Value  interface{} `mapstructure:"value" json:"value,omitempty" gorm:"column:newvalue" bson:"value,omitempty" dynamodbav:"value,omitempty" firestore:"value,omitempty"`
 }
