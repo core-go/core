e0b31a384804bfb07fd3612973c87a83c0b506e7
Duc Nguyen <minhduc140583@gmail.com>
2020-10-25 15:44:41 +0700

    Add MakeArray

--- util.go
+++ util.go
@@ -13,6 +13,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -146,4 +147,31 @@ func Marshal(v interface{}) ([]byte, error) {
 		return []byte(s), nil
 	}
 	return json.Marshal(v)
+}
+
+func MakeDurations(vs []int64) []time.Duration {
+	durations := make([]time.Duration, 0)
+	for _, v := range vs {
+		d := time.Duration(v) * time.Second
+		durations = append(durations, d)
+	}
+	return durations
+}
+func MakeArray(v interface{}, prefix string, max int) []int64 {
+	var ar []int64
+	v2 := reflect.Indirect(reflect.ValueOf(v))
+	for i := 1; i <= max; i++ {
+		fn := prefix + strconv.Itoa(i)
+		v3 := v2.FieldByName(fn).Interface().(int64)
+		if v3 > 0 {
+			ar = append(ar, v3)
+		} else {
+			return ar
+		}
+	}
+	return ar
+}
+func DurationsFromValue(v interface{}, prefix string, max int) []time.Duration {
+	arr := MakeArray(v, prefix, max)
+	return MakeDurations(arr)
 }
