7c785bbc308a143496fc6f45f39ad6d160bd10f6
Duc Nguyen <minhduc140583@gmail.com>
2021-05-29 14:55:02 +0700

    Create strings package

---
+++ config.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"reflect"
+	"strconv"
+	"time"
+)
+
+func MakeDurations(vs []int64) []time.Duration {
+	durations := make([]time.Duration, 0)
+	for _, v := range vs {
+		d := time.Duration(v) * time.Second
+		durations = append(durations, d)
+	}
+	return durations
+}
+func MakeArray(v interface{}, prefix string, max int) []int64 {
+	var ar []int64
+	v2 := reflect.Indirect(reflect.ValueOf(v))
+	for i := 1; i <= max; i++ {
+		fn := prefix + strconv.Itoa(i)
+		v3 := v2.FieldByName(fn).Interface().(int64)
+		if v3 > 0 {
+			ar = append(ar, v3)
+		} else {
+			return ar
+		}
+	}
+	return ar
+}
+func DurationsFromValue(v interface{}, prefix string, max int) []time.Duration {
+	arr := MakeArray(v, prefix, max)
+	return MakeDurations(arr)
+}
