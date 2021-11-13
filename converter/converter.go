182706a27d376c3c184afae2c0899683b343595c
Duc Nguyen <minhduc140583@gmail.com>
2021-11-13 10:44:33 +0700

    Fix bug DateToUnixNano

--- converter/converter.go
+++ converter/converter.go
@@ -5,42 +5,42 @@ import (
 	"time"
 )
 
-func TimeToMilliseconds(sTime string) (int64, error) {
+func TimeToMilliseconds(time string) (int64, error) {
 	var h, m, s int
-	_, err := fmt.Sscanf(sTime, "%02d:%02d:%02d", &h, &m, &s)
+	_, err := fmt.Sscanf(time, "%02d:%02d:%02d", &h, &m, &s)
 	if err != nil {
 		return 0, err
 	}
 	return int64(h * 3600000 + m * 60000 + s * 1000), nil
 }
-func DateToUnixTime(sDate string) (int64, error) {
-	layout := "2006:01:02"
-	date, err := time.Parse(layout, sDate)
+func DateToUnixTime(s string) (int64, error) {
+	layout := "2006-01-02"
+	date, err := time.Parse(layout, s)
 	if err != nil {
 		return 0, err
 	}
 	return date.Unix() * 1000, nil
 }
-func DateToUnixNano(sDate string) (int64, error) {
-	layout := "2006:01:02"
-	date, err := time.Parse(layout, sDate)
+func DateToUnixNano(s string) (int64, error) {
+	layout := "2006-01-02"
+	date, err := time.Parse(layout, s)
 	if err != nil {
 		return 0, err
 	}
 	return date.UnixNano(), nil
 }
-func UnixTime(milliseconds int64) string {
-	dateUtc := time.Unix(0, milliseconds* 1000000)
-	return dateUtc.Format("2006:01:02")
+func UnixTime(m int64) string {
+	dateUtc := time.Unix(0, m* 1000000)
+	return dateUtc.Format("2006-01-02")
 }
-func MillisecondsToTimeString(timeMilisecond int) string {
+func MillisecondsToTimeString(milliseconds int) string {
 	hourUint := 3600000 //60 * 60 * 1000 = 3600000
 	minuteUint := 60000 //60 * 1000 = 60000
 	secondUint := 1000
-	hour := timeMilisecond / hourUint
-	timeMilisecond = timeMilisecond % hourUint
-	minute := timeMilisecond / minuteUint
-	timeMilisecond = timeMilisecond % minuteUint
-	second := timeMilisecond / secondUint
+	hour := milliseconds / hourUint
+	milliseconds = milliseconds % hourUint
+	minute := milliseconds / minuteUint
+	milliseconds = milliseconds % minuteUint
+	second := milliseconds / secondUint
 	return fmt.Sprintf("%02d:%02d:%02d", hour, minute, second)
 }
